9455593e2e5ffc4022cdbb36e42008170be94b46
Landon Wills <landonjwdev@gmail.com>
2024-06-13 02:11:56 -0400

    Initial commit

---
+++ src/client/java/ca/landonjw/OpenCameraClient.java
@@ -0,0 +1,10 @@
+package ca.landonjw;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OpenCameraClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
